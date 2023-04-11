cf4eb858fbe7a0f461a3cd9b0b9880e4658c5d16
Jhon-crypt <johnoladele690@gmail.com>
2023-04-11 15:53:06 +0100

    Made some changes

--- src/components/section/howItWorksSection.tsx
+++ src/components/section/howItWorksSection.tsx
@@ -4,63 +4,67 @@ function HowItWorks() {
 
         <>
 
-            <section className="pt-24 pb-32 bg-white overflow-hidden">
+            <section className="pt-24 pb-32 overflow-hidden">
                 <div className="container px-4 mx-auto">
-                    <h2 className="mb-28 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight md:max-w-lg">We make things easy for your business</h2>
+                    <h2 className=" text-accent mb-28 text-6xl md:text-7xl font-bold font-heading tracking-px-n leading-tight md:max-w-lg">We make things easy for your business</h2>
                     <div className="flex flex-wrap -m-8">
-                        <div className="w-full md:w-1/2 lg:w-1/4 p-8">
+
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-8 hover:bg-base-100 hover:rounded-lg hover:drop-shadow-lg hover:text-primary">
                             <div className="flex flex-wrap items-center mb-7 -m-2">
                                 <div className="w-auto p-2">
-                                    <div className="relative w-14 h-14 text-2xl font-bold font-heading bg-indigo-100 rounded-full">
+                                    <div className="relative w-14 h-14 text-2xl font-bold font-heading bg-accent rounded-full hover:bg-primary hover:text-accent">
                                         <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">1</span>
                                     </div>
                                 </div>
                                 <div className="flex-1 p-2">
-                                    <div className="w-full h-px bg-gray-200"></div>
+                                    <div className="w-full h-px bg-gray-200 hover:bg-primary"></div>
                                 </div>
                             </div>
                             <h3 className="text-xl font-semibold leading-normal md:max-w-xs">A complete UI kit for building your business</h3>
                         </div>
-                        <div className="w-full md:w-1/2 lg:w-1/4 p-8">
+
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-8 hover:bg-base-100 hover:rounded-lg hover:drop-shadow-lg hover:text-secondary">
                             <div className="flex flex-wrap items-center mb-7 -m-2">
                                 <div className="w-auto p-2">
-                                    <div className="relative w-14 h-14 text-2xl text-white font-bold bg-indigo-600 rounded-full">
-                                        <img className="absolute top-0 left-0" src="flaro-assets/images/how-it-works/gradient.svg" alt=""/>
-                                            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">2</span>
+                                    <div className="relative w-14 h-14 text-2xl font-bold rounded-full bg-accent hover:bg-secondary hover:text-accent">
+                                            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">2</span>
                                     </div>
                                 </div>
                                 <div className="flex-1 p-2">
-                                    <div className="w-full h-px bg-gray-200"></div>
+                                    <div className="w-full h-px bg-gray-200 hover:bg-secondary"></div>
                                 </div>
                             </div>
                             <h3 className="text-xl font-semibold leading-normal md:max-w-xs">A complete UI kit for building your business</h3>
                         </div>
-                        <div className="w-full md:w-1/2 lg:w-1/4 p-8">
+
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-8 hover:bg-base-100 hover:rounded-lg hover:drop-shadow-lg hover:text-primary">
                             <div className="flex flex-wrap items-center mb-7 -m-2">
                                 <div className="w-auto p-2">
-                                    <div className="relative w-14 h-14 text-2xl font-bold font-heading bg-indigo-100 rounded-full">
+                                    <div className="relative w-14 h-14 text-2xl font-bold font-heading bg-accent rounded-full hover:bg-primary hover:text-accent">
                                         <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">3</span>
                                     </div>
                                 </div>
                                 <div className="flex-1 p-2">
-                                    <div className="w-full h-px bg-gray-200"></div>
+                                    <div className="w-full h-px bg-gray-200 hover:bg-primary"></div>
                                 </div>
                             </div>
                             <h3 className="text-xl font-semibold leading-normal md:max-w-xs">A complete UI kit for building your business</h3>
                         </div>
-                        <div className="w-full md:w-1/2 lg:w-1/4 p-8">
+
+                        <div className="w-full md:w-1/2 lg:w-1/4 p-8 hover:bg-base-100 hover:rounded-lg hover:drop-shadow-lg hover:text-secondary">
                             <div className="flex flex-wrap items-center mb-7 -m-2">
                                 <div className="w-auto p-2">
-                                    <div className="relative w-14 h-14 text-2xl font-bold font-heading bg-indigo-100 rounded-full">
-                                        <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">4</span>
+                                    <div className="relative w-14 h-14 text-2xl font-bold rounded-full bg-accent hover:bg-secondary hover:text-accent">
+                                            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">4</span>
                                     </div>
                                 </div>
                                 <div className="flex-1 p-2">
-                                    <div className="w-full h-px bg-gray-200"></div>
+                                    <div className="w-full h-px bg-gray-200 hover:bg-secondary"></div>
                                 </div>
                             </div>
                             <h3 className="text-xl font-semibold leading-normal md:max-w-xs">A complete UI kit for building your business</h3>
                         </div>
+
                     </div>
                 </div>
             </section>
